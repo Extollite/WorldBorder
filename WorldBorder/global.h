c42b248965ab07675bd8511a4e111122f984a2d9
Jedrzej <42713788+Extollite@users.noreply.github.com>
2020-05-21 14:03:15 +0200

    Add dimension support.

--- WorldBorder/global.h
+++ WorldBorder/global.h
@@ -10,25 +10,36 @@
 #include <Packet/TextPacket.h>
 #include <boost/algorithm/string/replace.hpp>
 
-
-struct Settings {
+struct WorldBorder {
   long long minX = -30000;
-  long long maxX = -30000;
+  long long maxX = 30000;
 
-  long long minZ = 30000;
+  long long minZ = -30000;
   long long maxZ = 30000;
 
+  template <typename IO> static inline bool io(IO f, WorldBorder &overworld, YAML::Node &node) {
+    return f(overworld.minX, node["minX"]) && f(overworld.minZ, node["minZ"]) && f(overworld.maxX, node["maxX"]) &&
+           f(overworld.maxZ, node["maxZ"]);
+  }
+};
+
+struct Settings {
   int informBefore = 10;
 
-  std::string messageBorder = "You've reached world border!";
+  std::string messageBorder         = "You've reached world border!";
   std::string messageBorderApproach = "You will reach border at %coord% pos in %number% blocks!";
 
+  WorldBorder overworld;
+
+  WorldBorder nether;
+
+  WorldBorder end;
 
   template <typename IO> static inline bool io(IO f, Settings &settings, YAML::Node &node) {
-    return f(settings.minX, node["minX"]) && f(settings.minZ, node["minZ"]) && f(settings.maxX, node["maxX"]) &&
-           f(settings.maxZ, node["maxZ"]) && f(settings.messageBorder, node["reachBorderMessage"]) &&
+    return f(settings.messageBorder, node["reachBorderMessage"]) &&
            f(settings.messageBorderApproach, node["messageBorderApproach"]) &&
-           f(settings.informBefore, node["informBefore"]);
+           f(settings.informBefore, node["informBefore"]) && f(settings.overworld, node["overworld"]) &&
+           f(settings.nether, node["nether"]) && f(settings.end, node["end"]);
   }
 };
 
--- WorldBorder/main.cpp
+++ WorldBorder/main.cpp
@@ -8,8 +8,15 @@ DEFAULT_SETTINGS(settings);
 void dllenter() {}
 void dllexit() {}
 
-void PreInit() {}
+std::unordered_map<int, WorldBorder> borders;
+
+void PreInit() { borders[0] = settings.overworld;
+  borders[1] = settings.nether;
+  borders[2]               = settings.end;
+}
 void PostInit() {}
+
+
 
 void teleport(Player& thi, float x, float y, float z) {
   Vec3 teleportTo;
@@ -36,25 +43,32 @@ THook(void *, "?move@Player@@UEAAXAEBVVec3@@@Z", Player &thi, Vec3 const &newPos
   Vec3 currPos = thi.getPos();
   if (std::fabs(currPos.x - prevPos.x) >= 0.00001 ||
       std::fabs(currPos.z - prevPos.z) >= 0.00001) {
-    if (currPos.x >= settings.maxX) {
+    WorldBorder border = borders[thi.getDimensionId().value];
+    if (currPos.x >= border.maxX) {
       teleport(thi, prevPos.x - 1, prevPos.y, prevPos.z);
-    } else if (currPos.x <= settings.minX) {
+    } else if (currPos.x <= border.minX) {
       teleport(thi, prevPos.x + 1, prevPos.y, prevPos.z);
-    } else if (currPos.z >= settings.maxZ) {
+    } else if (currPos.z >= border.maxZ) {
       teleport(thi, prevPos.x, prevPos.y, prevPos.z - 1);
-    } else if (currPos.z <= settings.minZ) {
+    } else if (currPos.z <= border.minZ) {
       teleport(thi, prevPos.x, prevPos.y, prevPos.z + 1);
     }
     
-    if (std::fabs(settings.maxX - currPos.x) <= settings.informBefore+1) {
-      reachMsg(thi, "X", std::fabs(settings.maxX - currPos.x));
-    } else if (std::fabs(settings.minX - currPos.x) <= settings.informBefore+1) {
-      reachMsg(thi, "X", std::fabs(settings.minX - currPos.x));
-    } else if (std::fabs(settings.maxZ - currPos.z) <= settings.informBefore+1) {
-      reachMsg(thi, "Z", std::fabs(settings.maxZ - currPos.z));
-    } else if (std::fabs(settings.minZ - currPos.z) <= settings.informBefore+1) {
-      reachMsg(thi, "Z", std::fabs(settings.minZ - currPos.z));
+    std::string coord = "X";
+    float min         = std::fabs(border.maxX - currPos.x);
+    if (std::fabs(border.minX - currPos.x) < min) {
+      min = std::fabs(border.minX - currPos.x);
     }
+    if (std::fabs(border.maxZ - currPos.z) < min) {
+      min = std::fabs(border.maxZ - currPos.z);
+      coord = "Z";
+    }
+    if (std::fabs(border.minZ - currPos.z) < min) {
+      min   = std::fabs(border.minZ - currPos.z);
+      coord = "Z";
+    }
+    if (min <= settings.informBefore + 1)
+        reachMsg(thi, coord, min);
   }
   return ret;
 }
@@ -62,7 +76,8 @@ THook(void *, "?move@Player@@UEAAXAEBVVec3@@@Z", Player &thi, Vec3 const &newPos
 THook(bool, "?teleportTo@Player@@UEAAXAEBVVec3@@_NHH@Z", Player &thi, Vec3 const & pos, bool b1, int i1, int i2) {
   if (thi.getCommandPermissionLevel() > CommandPermissionLevel::Any) return original(thi, pos, b1, i1, i2);
   Vec3 prevPos = thi.getPos();
-  if (pos.x >= settings.maxX || pos.x <= settings.minX || pos.z >= settings.maxZ || pos.z <= settings.minZ) {
+  WorldBorder border = borders[thi.getDimensionId().value];
+  if (pos.x >= border.maxX || pos.x <= border.minX || pos.z >= border.maxZ || pos.z <= border.minZ) {
     teleport(thi, prevPos.x, prevPos.y, prevPos.z);
     return false;
   }
